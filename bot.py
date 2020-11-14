b78c5fa32a50dd27886f8fbc991432f3cfff0722
keinoa <74367200+keinoa@users.noreply.github.com>
2020-11-15 00:08:06 +0800

    20201115

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord
+from discord.shard import EventItem
+From discord.ext import commands
+
+bot = commands.Bot(commands_prefix='!')
+
+@bot.event
+async def on_ready()
+    print(">> Bot is online <<")
+
+bot run("NDU5Njc5NjI3Njk5NDIxMTg0.WyzcAw.iR01e4Yjd-yXv2EXQaEjILD8rgs")
